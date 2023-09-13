13735f0385381fb906c287d09c781773bef5a405
Camila <by.cmlla0107@gmail.com>
2023-09-13 16:24:14 -0300

    remove one

--- Testesdevisãoestéreo/capture-videos/video01.py
+++ Testesdevisãoestéreo/capture-videos/video01.py
@@ -1,6 +1,6 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
 while True:
     ret, frame = cap.read()
 
